6c2caefbef7073f948f2557b4b06c204829df8ce
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-01-18 22:28:15 +0300

    step 4

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,6 +5,17 @@ require_relative "hexlet_code/version"
 # Main module
 module HexletCode
   autoload :Tag, "hexlet_code/tag"
+
+  def self.form_for(_, **params)
+    inner_params = ["action=\"#{params.fetch(:url, "#")}\""]
+    params.delete(:url)
+    inner_params << "method=\"#{params.fetch(:method, "post")}\""
+    params.delete(:method)
+    inner_params = [*inner_params, *params.map { |k, v| "#{k.to_s.gsub("_", "-")}=\"#{v}\"" }]
+    res = "<form #{inner_params.join(" ")}></form>"
+    yield res
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class TestHexletCode < Minitest::Test
+class TestHexletCode < Minitest::Test # rubocop:disable Metrics/ClassLength
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
@@ -14,5 +14,204 @@ class TestHexletCode < Minitest::Test
     assert_equal (HexletCode::Tag.build("label") { "Email" }), "<label>Email</label>"
     assert_equal (HexletCode::Tag.build("label", for: "email") { "Email" }), '<label for="email">Email</label>'
     assert_equal HexletCode::Tag.build("div"), "<div></div>"
+  end
+
+  User = Struct.new(:name, :job, :email, keyword_init: true)
+
+  def test_basic_form
+    user = User.new(name: "rob")
+    expected = '<form action="#" method="post"></form>'
+    result = nil
+    HexletCode.form_for(user) { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_class
+    user = User.new(name: "rob")
+    expected = '<form action="#" method="post" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_url
+    user = User.new(name: "rob")
+    expected = '<form action="/profile" method="post"></form>'
+    result = nil
+    HexletCode.form_for(user, url: "/profile") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_url_and_class
+    user = User.new(name: "rob")
+    expected = '<form action="/profile" method="post" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, url: "/profile", class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_method_get
+    user = User.new(name: "rob")
+    expected = '<form action="#" method="get"></form>'
+    result = nil
+    HexletCode.form_for(user, method: "get") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_method_get_and_class
+    user = User.new(name: "rob")
+    expected = '<form action="#" method="get" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, method: "get", class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_method_get_and_url
+    user = User.new(name: "rob")
+    expected = '<form action="/profile" method="get"></form>'
+    result = nil
+    HexletCode.form_for(user, method: "get", url: "/profile") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_method_get_url_and_class
+    user = User.new(name: "rob")
+    expected = '<form action="/profile" method="get" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, method: "get", url: "/profile", class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_id
+    user = User.new(name: "rob")
+    expected = '<form action="#" method="post" id="user-form"></form>'
+    result = nil
+    HexletCode.form_for(user, id: "user-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_id_and_class
+    user = User.new(name: "rob")
+    expected = '<form action="#" method="post" id="user-form" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, id: "user-form", class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_id_and_url
+    user = User.new(name: "rob")
+    expected = '<form action="/profile" method="post" id="user-form"></form>'
+    result = nil
+    HexletCode.form_for(user, id: "user-form", url: "/profile") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_id_url_and_class
+    user = User.new(name: "rob")
+    expected = '<form action="/profile" method="post" id="user-form" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, id: "user-form", url: "/profile", class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_id_method_get
+    user = User.new(name: "rob")
+    expected = '<form action="#" method="get" id="user-form"></form>'
+    result = nil
+    HexletCode.form_for(user, id: "user-form", method: "get") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_id_method_get_and_class
+    user = User.new(name: "rob")
+    expected = '<form action="#" method="get" id="user-form" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, id: "user-form", method: "get", class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_id_method_get_and_url
+    user = User.new(name: "rob")
+    expected = '<form action="/profile" method="get" id="user-form"></form>'
+    result = nil
+    HexletCode.form_for(user, id: "user-form", method: "get", url: "/profile") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_id_method_get_url_and_class
+    user = User.new(name: "rob")
+    expected = '<form action="/profile" method="get" id="user-form" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, id: "user-form", method: "get", url: "/profile", class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_data_attribute
+    user = User.new(name: "rob")
+    expected = '<form action="#" method="post" data-remote="true"></form>'
+    result = nil
+    HexletCode.form_for(user, data_remote: true) { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_data_attribute_and_class
+    user = User.new(name: "rob")
+    expected = '<form action="#" method="post" data-remote="true" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, data_remote: true, class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_data_attribute_and_url
+    user = User.new(name: "rob")
+    expected = '<form action="/profile" method="post" data-remote="true"></form>'
+    result = nil
+    HexletCode.form_for(user, data_remote: true, url: "/profile") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_data_attribute_url_and_class
+    user = User.new(name: "rob")
+    expected = '<form action="/profile" method="post" data-remote="true" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, data_remote: true, url: "/profile", class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_additional_fields
+    user = User.new(name: "rob", job: "developer", email: "rob@example.com")
+    expected = '<form action="#" method="post"></form>'
+    result = nil
+    HexletCode.form_for(user) { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_additional_fields_and_class
+    user = User.new(name: "rob", job: "developer", email: "rob@example.com")
+    expected = '<form action="#" method="post" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_additional_fields_and_url
+    user = User.new(name: "rob", job: "developer", email: "rob@example.com")
+    expected = '<form action="/profile" method="post"></form>'
+    result = nil
+    HexletCode.form_for(user, url: "/profile") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_with_additional_fields_url_and_class
+    user = User.new(name: "rob", job: "developer", email: "rob@example.com")
+    expected = '<form action="/profile" method="post" class="hexlet-form"></form>'
+    result = nil
+    HexletCode.form_for(user, url: "/profile", class: "hexlet-form") { |f| result = f }
+    assert_equal expected, result
+  end
+
+  def test_form_without_block
+    user = User.new(name: "rob")
+    assert_raises(LocalJumpError) { HexletCode.form_for(user) }
   end
 end
